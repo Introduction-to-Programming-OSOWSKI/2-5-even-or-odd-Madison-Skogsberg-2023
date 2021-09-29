146061629d77d9d740ef3af6164c8b5ea9fefc2b
Madison-Skogsberg-2023 <816418@mystma.org>
2021-09-29 09:32:51 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+#define function
+#add parameters
+def isEven(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+#run function
+print(isEven(10))
